e0114cef24ec9fdb158f8b31992592ee23219dbf
Simon Hloska <hloska@spsejecna.cz>
2024-05-08 15:19:35 +0200

    Porbably fixed the bug by deleting the whole panel and creating other one for each thing

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -11,22 +11,19 @@ public class MyPanel extends JPanel implements ActionListener {
     private JButton rightVent = new JButton(),
             leftVent = new JButton(), rightDoor = new JButton(), leftDoor = new JButton();
     private Font myFont = new Font("Arial", Font.BOLD, 8);
-    private final int PANEL_WIDTH = 1000;
-    private final int PANEL_HEIGHT = 600;
+    private JPanel currentPanel;
+    private final int PANEL_WIDTH = 1000, PANEL_HEIGHT = 600;
     private ArrayList<Riddles> riddles = new ArrayList<>();
     private ArrayList<Puzzles> puzzles = new ArrayList<>();
     private Image playerImg, backgroundImg, chessPuzzle;
     private Timer timer;
-    private int startX = 700;
-    private int startY = 500;
-    private int endX;
-    private int endY;
-    private int randomIndex;
-    private String riddle, answer;
+    private int endX, endY, randomIndex, startX = 700, startY = 500;
+    private String riddle, answer, puzzleText;
     private PlayerStats ps = new PlayerStats();
-    private final int numFrames = 50+(100- ps.getStamina());
+    private final int numFrames = 50 + (100 - ps.getStamina());
     private MainFunctions mf;
-    public void my1floorPanel(){
+
+    public void my1floorPanel() {
         leftDoor = createButton("Left Door", 40, 350, leftDoor);
         rightDoor = createButton("Right Door", 900, 250, rightDoor);
         leftVent = createButton("Left Vent", 200, 436, leftVent);
@@ -35,65 +32,73 @@ public class MyPanel extends JPanel implements ActionListener {
         createBackground("1stFloor.jpg");
         createPlayer();
         this.setVisible(true);
+        setCurrentPanel(this);
         repaint();
     }//is finished
-    public void my2floorPanel(){
-        changePosition(leftDoor, 626,349);
-        changePosition(rightDoor,969,397);
-        changePosition(leftVent, 36,470);
-        changePosition(rightVent, 747,478);
+
+    public void my2floorPanel() {
+        changePosition(leftDoor, 626, 349);
+        changePosition(rightDoor, 969, 397);
+        changePosition(leftVent, 36, 470);
+        changePosition(rightVent, 747, 478);
         createBackground("2stFloor.png");
         createPlayer();
         this.setVisible(true);
         repaint();
     }//is finished
-    public void my3floorPanel(){
-        changePosition(leftDoor, 626,349);
-        changePosition(rightDoor,969,397);
-        changePosition(leftVent, 36,470);
-        changePosition(rightVent, 747,478);
-        createBackground("2stFloor.png");
-        createPlayer();
-        this.setVisible(true);
-        repaint();
-    }//isnt finished
-    public void my4floorPanel(){
-        changePosition(leftDoor, 626,349);
-        changePosition(rightDoor,969,397);
-        changePosition(leftVent, 36,470);
-        changePosition(rightVent, 747,478);
-        createBackground("2stFloor.png");
-        createPlayer();
-        this.setVisible(true);
-        repaint();
-    }//isnt finished
-    public void my5floorPanel(){
-        changePosition(leftDoor, 626,349);
-        changePosition(rightDoor,969,397);
-        changePosition(leftVent, 36,470);
-        changePosition(rightVent, 747,478);
+
+    public void my3floorPanel() {
+        changePosition(leftDoor, 626, 349);
+        changePosition(rightDoor, 969, 397);
+        changePosition(leftVent, 36, 470);
+        changePosition(rightVent, 747, 478);
         createBackground("2stFloor.png");
         createPlayer();
         this.setVisible(true);
         repaint();
     }//isnt finished
 
-    public void addButtons(JButton button1, JButton button2,JButton button3,JButton button4){
+    public void my4floorPanel() {
+        changePosition(leftDoor, 626, 349);
+        changePosition(rightDoor, 969, 397);
+        changePosition(leftVent, 36, 470);
+        changePosition(rightVent, 747, 478);
+        createBackground("2stFloor.png");
+        createPlayer();
+        this.setVisible(true);
+        repaint();
+    }//isnt finished
+
+    public void my5floorPanel() {
+        changePosition(leftDoor, 626, 349);
+        changePosition(rightDoor, 969, 397);
+        changePosition(leftVent, 36, 470);
+        changePosition(rightVent, 747, 478);
+        createBackground("2stFloor.png");
+        createPlayer();
+        this.setVisible(true);
+        repaint();
+    }//isnt finished
+
+    public void addButtons(JButton button1, JButton button2, JButton button3, JButton button4) {
         this.add(button1);
         this.add(button2);
         this.add(button3);
         this.add(button4);
     }
-    public void createPlayer(){
+
+    public void createPlayer() {
         playerImg = new ImageIcon("playerIcon.png").getImage();
         playerImg = playerImg.getScaledInstance(52, 52, Image.SCALE_SMOOTH);
         playerImg = new ImageIcon(playerImg).getImage();
     }
-    public void createBackground(String filePath){
+
+    public void createBackground(String filePath) {
         backgroundImg = new ImageIcon(filePath).getImage();
         backgroundImg = backgroundImg.getScaledInstance(PANEL_WIDTH, PANEL_HEIGHT, Image.SCALE_SMOOTH);
         backgroundImg = new ImageIcon(backgroundImg).getImage();
     }
+
     public MyPanel() {
         this.setLayout(null);
         this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
@@ -109,6 +114,24 @@ public class MyPanel extends JPanel implements ActionListener {
         timer = new Timer(animationDuration / numFrames, this);
     }
 
+    private void displayRiddle(String riddle, String answer) {
+        RiddlePanel riddlePanel = new RiddlePanel(riddle, answer, PANEL_WIDTH, PANEL_HEIGHT);
+        setCurrentPanel(riddlePanel);
+    }
+
+    private void displayChessPuzzle(Image image, String text) {
+        ChessPuzzlesPanel puzzlePanel = new ChessPuzzlesPanel(image, text, PANEL_WIDTH, PANEL_HEIGHT);
+        setCurrentPanel(puzzlePanel);
+    }
+
+    private void setCurrentPanel(JPanel panel) {
+        if (currentPanel != null) {
+            currentPanel.removeAll();
+        }
+        currentPanel = panel;
+        repaint();
+    }
+
     private JButton createButton(String text, int x, int y, JButton button) {
         button.setFont(myFont);
         button.addMouseListener(new MouseAdapter() {
@@ -117,6 +140,7 @@ public class MyPanel extends JPanel implements ActionListener {
                 button.setBackground(Color.WHITE);
                 button.setForeground(Color.BLACK);
             }
+
             @Override
             public void mouseExited(MouseEvent e) {
                 button.setBackground(Color.BLACK);
@@ -131,10 +155,10 @@ public class MyPanel extends JPanel implements ActionListener {
         button.setVisible(true);
         return button;
     }
-    private void changePosition(JButton button, int x, int y){
+
+    private void changePosition(JButton button, int x, int y) {
         button.setBounds(x, y, 70, 30);
     }
-
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2D = (Graphics2D) g;
@@ -144,37 +168,29 @@ public class MyPanel extends JPanel implements ActionListener {
 
             int xPos = 100;
             int yPos = 50;
-
-            g2D.setColor(Color.WHITE);
-            g2D.setStroke(new BasicStroke());
-            g2D.clearRect(0,0,1000,600);
             g2D.drawString(puzzles.get(randomIndex).getQuestion(), xPos + 150, yPos - 30);
             g2D.drawImage(chessPuzzle, xPos, yPos, null);
             chessPuzzle = null;
-        }else if(riddle != null){
+        } else if (riddle != null) {
             hideButtons();
 
             int xPos = 100;
             int yPos = 50;
-
-            g2D.setColor(Color.WHITE);
-            g2D.setStroke(new BasicStroke());
-            g2D.clearRect(0,0,1000,600);
             g2D.drawString(riddle, xPos, yPos);
             riddle = null;
-        }else{
+        } else {
             g2D.drawImage(backgroundImg, 0, 0, null);
             g2D.drawString("You", startX + 13, startY - 5);
             g2D.drawImage(playerImg, startX, startY, null);
         }
     }
-    public void hideButtons(){
+    public void hideButtons() {
         leftDoor.setVisible(false);
         leftVent.setVisible(false);
         rightVent.setVisible(false);
         rightDoor.setVisible(false);
     }
-    public void showButtons(){
+    public void showButtons() {
         leftDoor.setVisible(true);
         leftVent.setVisible(true);
         rightVent.setVisible(true);
@@ -206,22 +222,28 @@ public class MyPanel extends JPanel implements ActionListener {
         randomIndex = rd.nextInt(puzzles.size() - 1);
         chessPuzzle = puzzles.get(randomIndex).getImg().getScaledInstance(PANEL_HEIGHT - 200, PANEL_HEIGHT - 200, Image.SCALE_SMOOTH);
         chessPuzzle = new ImageIcon(chessPuzzle).getImage();
-        repaint();
+        puzzleText = puzzles.get(randomIndex).getQuestion();
+        Rectangle backgroundRect = new Rectangle(0, 0, PANEL_WIDTH, PANEL_HEIGHT);
+        displayChessPuzzle(chessPuzzle, puzzleText);
+        repaint(backgroundRect);
     }
-    public void getRiddle(){
+
+    public void getRiddle() {
         loadRiddle();
+        Random rd = new Random();
+        randomIndex = rd.nextInt(riddles.size() - 1);
         riddle = riddles.get(randomIndex).getRiddle();
         answer = riddles.get(randomIndex).getAnswer();
+        displayRiddle(riddle, answer);
+        repaint();
     }
 
     public void loadRiddle() {
-        Random rd = new Random();
         riddles.add(new Riddles("What is something that you earn, but can also save and spend?", "money"));
         riddles.add(new Riddles("What is something that can grow over time, but needs to be managed carefully?", "investments"));
         riddles.add(new Riddles("What is something that can protect you from unexpected financial emergencies?", "insurance"));
         riddles.add(new Riddles("What is something that you must do regularly to keep track of your income and expenses?", "budgeting"));
         riddles.add(new Riddles("What is something that can help you achieve your financial goals, but requires patience and discipline?", "saving"));
-        randomIndex = rd.nextInt(riddles.size() - 1);
     }
 
     @Override
@@ -248,9 +270,9 @@ public class MyPanel extends JPanel implements ActionListener {
             Random random = new Random();
             boolean randomBoolean = random.nextBoolean();
             timer.stop();
-            if(randomBoolean)
+            if (randomBoolean)
                 getChessPuzzle();
-            else{
+            else {
                 getRiddle();
             }
 
